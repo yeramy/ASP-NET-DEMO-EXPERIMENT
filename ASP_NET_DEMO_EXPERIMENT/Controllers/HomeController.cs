b28de573139d44cfa7c0bb9fabfd61113409be42
DESKTOP-2082DFG\Me <yeramhwang@fnsusa.com>
2022-06-23 15:37:11 -0700

    First commit for project creation

---
+++ ASP_NET_DEMO_EXPERIMENT/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ASP_NET_DEMO_EXPERIMENT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_NET_DEMO_EXPERIMENT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP_NET_DEMO_EXPERIMENT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_NET_DEMO_EXPERIMENT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
