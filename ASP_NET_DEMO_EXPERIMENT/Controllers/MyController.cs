9ea4a3b66f4afd87ae0bbd0495a6dd6b357ad13f
DESKTOP-2082DFG\Me <yeramhwang@fnsusa.com>
2022-06-26 21:10:09 -0700

    Completion of MVC part

---
+++ ASP_NET_DEMO_EXPERIMENT/Controllers/MyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASP_NET_DEMO_EXPERIMENT.Models;
+
+namespace ASP_NET_DEMO_EXPERIMENT.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            VocabViewModel list = new VocabViewModel() {Word="SSD",Definition="Solid State Drive"};
+            return View(list);
+        }
+
+        public IActionResult showVocabList()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ASP_NET_DEMO_EXPERIMENT/Models/VocabViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP_NET_DEMO_EXPERIMENT.Models
+{
+    public class VocabViewModel
+    {
+        public string Word { get; set; }
+        public string Definition { get; set; }
+    }
+}
